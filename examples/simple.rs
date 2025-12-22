e7d53b46a5c07848be8d0a55ca6b7a8c594d71d2
Anders Musikka <anders@andersmusikka.se>
2025-12-22 12:05:44 +0100

    wip

---
+++ examples/simple.rs
@@ -0,0 +1,33 @@
+use safeselect::{safe_select, safe_select_context};
+
+#[tokio::main]
+async fn main() {
+    safe_select_context!(Simple (value: String));
+
+    {
+        println!("select");
+        safe_select!(
+            Simple,
+            conn(value)(
+                {
+                    // TODO: Figure out if Capture is protected enough that a user
+                    // can't smuggle one out in unsafe code and violate the rules for
+                    // UnsafeCell access
+                    *value? = "Hello".to_string();
+                    let mut value_moved_into_future = "World".to_string();
+                },
+                async | | {
+                    println!("Message: {}", value_moved_into_future);
+
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                    44u64
+                },
+                |conn| {
+                    println!("Continue: {:?}, {:?}", value, conn);
+                    Some(())
+                }
+            ),
+        )
+        .await;
+    }
+}
